05dc844d63fecfcccb9121287f52511bb4bacfd0
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-10-01 12:00:55 +0800

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <a href='/'>Home</a>
+                <a href='/new'>New</a>
+            </div>
+        );
+    }
+}
+
+export default Nav
